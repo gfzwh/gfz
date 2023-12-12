55cb7cb659cb0cebe523d776b867271bfd14216a
tets <tets@gmail.com>
2023-12-12 21:25:03 +0800

    fix: del test

--- test/tcpconnode/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nohup ./tcpconnode > /var/log/node/tcpconnode.log 2>&1 &
--- test/tcpconnode/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-Proc=`ps -ef |grep -w "./tcpconnode" |grep -v grep|wc -l`
-if [ $Proc -le 0 ];then
-    echo "Node havn't running .. "
-else
-    ps -ef | grep "./tcpconnode" | grep -v grep | awk '{print $2}' | xargs kill -9
-fi
